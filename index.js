f77b716c1c80d2f7eb714324e4839a189d8979f7
t.polozov <39823227+PolozovT@users.noreply.github.com>
2019-01-23 23:14:55 +0300

    added new answe r(twitter/gismeteo) by bot

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ const optQuestion1 = {
     })
 };
 
+const optQuestion2 = {
+    parse_mode: 'markdown',
+    disable_web_page_preview: false,
+    reply_markup:JSON.stringify({
+        inline_keyboard: [
+            [{text: 'twitter', callback_data: 'twitter'},
+                {text: 'weather', callback_data: 'weather'}]
+        ]
+    })
+};
+
 bot.on('text', function (msg) {     //msg - Объект, который возвращает этот метод, когда приходит сообщение,
 	//посмотрите через консоль из чего он состоит.
 
@@ -28,7 +39,7 @@ bot.on('text', function (msg) {     //msg - Объект, который воз�
 
   if ((messageText ==='Го') || (messageText ==='го')) {
 
-     bot.sendMessage(messageChatId, 'Вам кого?', optQuestion1);
+     bot.sendMessage(messageChatId, 'Who you need?', optQuestion1);
 
      bot.sendPhoto(messageChatId, urlPhoto);
   }
@@ -40,14 +51,33 @@ bot.on('callback_query', function (msg) {
 
     if (msg.data === 'you') {
         console.log("YOU");
-        answer1 = "YOU";
+        answer1 = "What?";
+        bot.sendMessage(msg.from.id, answer1, optQuestion2);
     }
 
     if (msg.data === "nobody") {
         console.log("NOBODY");
-        answer1 = "NOBODY";
+        answer1 = "BB";
+        bot.sendMessage(msg.from.id, answer1);
     }
-
-    bot.sendMessage(msg.from.id, answer1);
     
 });
+
+
+bot.on('callback_query', function (msg) {
+
+
+    if (msg.data === 'twitter') {
+        console.log("TWITTER");
+        answerTwitter = "twinkler";
+        bot.sendMessage(msg.from.id, answerTwitter);
+    }
+
+    if (msg.data === "weather") {
+        console.log("WEATHER");
+        urlGismeteo = "https://www.gismeteo.com";
+        bot.sendMessage(msg.from.id, urlGismeteo);
+    }
+
+});
+
