1e4268aefe246009a191f3c328f06a90220fa591
t.polozov <39823227+PolozovT@users.noreply.github.com>
2019-01-24 21:17:45 +0300

    some changes fith answers

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ process.env["NTBA_FIX_319"] = 1;
 
 var TelegramBot = require('node-telegram-bot-api');
 
+var Twit = require('twit');
+
 var TOKEN = '756950534:AAEIZHfD-ejbXkvOdNqRcRBPIbQnlHFKs7Y';
 
 var bot = new TelegramBot(TOKEN, {polling: true });
+/*
+var T = new Twit({
 
+});
+*/
 const urlPhoto = "https://memepedia.ru/wp-content/uploads/2016/08/fdKZ-ZpN7iw.jpg";
 
 const optQuestion1 = {
@@ -14,7 +20,9 @@ const optQuestion1 = {
     reply_markup:JSON.stringify({
         inline_keyboard: [
             [{text: 'you', callback_data: 'you'},
-                {text: 'nobody', callback_data: 'nobody'}]
+                {text: 'nobody', callback_data: 'nobody'}],
+            [{text: 'weather', callback_data: 'weather'},
+                {text: 'twitter', callback_data: 'twitter'}]
         ]
     })
 };
@@ -24,8 +32,8 @@ const optQuestion2 = {
     disable_web_page_preview: false,
     reply_markup:JSON.stringify({
         inline_keyboard: [
-            [{text: 'twitter', callback_data: 'twitter'},
-                {text: 'weather', callback_data: 'weather'}]
+            [{text: 'What are you need?', callback_data: 'What'},
+                {text: 'Gi', callback_data: 'gi'}]
         ]
     })
 };
@@ -39,9 +47,10 @@ bot.on('text', function (msg) {     //msg - Объект, который воз�
 
   if ((messageText ==='Го') || (messageText ==='го')) {
 
-     bot.sendMessage(messageChatId, 'Who you need?', optQuestion1);
+      bot.sendPhoto(messageChatId, urlPhoto);
 
-     bot.sendPhoto(messageChatId, urlPhoto);
+      bot.sendMessage(messageChatId, 'Who you need?', optQuestion1);
+
   }
 
 });
@@ -60,23 +69,34 @@ bot.on('callback_query', function (msg) {
         answer1 = "BB";
         bot.sendMessage(msg.from.id, answer1);
     }
-    
+
+    if (msg.data === "weather") {
+        console.log("WEATHER");
+        urlGismeteo = "https://www.gismeteo.ru/weather-moscow-4368/3-days";
+        bot.sendMessage(msg.from.id, urlGismeteo);
+    }
+
+    if (msg.data === "twitter") {
+        console.log("TWITTER");
+        urlTwitter = "https://www.twitter.com";
+        bot.sendMessage(msg.from.id, urlTwitter);
+    }
 });
 
 
 bot.on('callback_query', function (msg) {
 
 
-    if (msg.data === 'twitter') {
-        console.log("TWITTER");
-        answerTwitter = "twinkler";
-        bot.sendMessage(msg.from.id, answerTwitter);
+    if (msg.data === 'What') {
+        console.log("WHAT");
+        answerWhat = "What?";
+        bot.sendMessage(msg.from.id, answerWhat);
     }
 
-    if (msg.data === "weather") {
-        console.log("WEATHER");
-        urlGismeteo = "https://www.gismeteo.com";
-        bot.sendMessage(msg.from.id, urlGismeteo);
+    if (msg.data === 'gi') {
+        console.log("GI");
+        answerGi = "Gi";
+        bot.sendMessage(msg.from.id, answerGi);
     }
 
 });
